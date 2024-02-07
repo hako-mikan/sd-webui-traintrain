d22a1c8a88ee3c7ca03b2bfea69a70c2438f964e
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2024-02-07 12:31:44 +0900

    for forge

--- install.py
+++ install.py
@@ -24,14 +24,17 @@ for module in requirements:
     if not launch.is_installed(module):
         launch.run_pip(f"install {module}", module)
 
-if os.name == 'nt':
-    package_url = "https://github.com/Keith-Hon/bitsandbytes-windows.git"
-    package_name = "bitsandbytes" 
-else:
-    package_url = "bitsandbytes"
-    package_name = "bitsandbytes"
+try:
+    from ldm_patched.modules import model_management
+except:
+    if os.name == 'nt':
+        package_url = "https://github.com/Keith-Hon/bitsandbytes-windows.git"
+        package_name = "bitsandbytes" 
+    else:
+        package_url = "bitsandbytes"
+        package_name = "bitsandbytes"
 
-if not launch.is_installed(package_name):
-    launch.run_pip(f"install { 'git+' + package_url if 'http' in package_url else package_url}", package_name)
-else:
-    print(f"{package_name} is already installed.")
+    if not launch.is_installed(package_name):
+        launch.run_pip(f"install { 'git+' + package_url if 'http' in package_url else package_url}", package_name)
+    else:
+        print(f"{package_name} is already installed.")
